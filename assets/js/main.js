9af59f1f0b4382ae951d2339b17fd8fe6854bf45
Giuditta <giudittamarino89@gmail.com>
2021-04-08 17:25:25 +0200

    bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,8 @@ var app = new Vue({
     showName: function(){
       for (var i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-          email= {
-            emailAdress: response.data.response
-          }
-          this.emails.push(email);
+        .then((response) => {          
+          this.emails.push(response.data.response);
         });
       }
     console.log(this.emails);
